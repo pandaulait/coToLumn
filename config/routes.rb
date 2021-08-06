32a3af766dd3b8c4e68b1a93a88597134117c5b1
pandaulait <show.hey.panda@gmail.com>
2021-08-06 04:46:56 +0000

    [Update]アカウント登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+  devise_for :users, controllers: {
+    sessions:      'public/devise/sessions',
+    passwords:     'public/devise/passwords',
+    registrations: 'public/devise/registrations'
+  }
+  devise_for :admins,
+  path: :admin,
+  controllers: {
+    sessions: 'admin/devise/sessions',
+    passwords: 'admin/devise/passwords',
+    registrations: 'admin/devise/registrations',
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
