bcf49b57c2a24ee13659634dc8d9fac5c0b1b38e
pandaulait <show.hey.panda@gmail.com>
2021-08-29 11:08:28 +0000

    [Update]top

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,12 @@
 class HomesController < ApplicationController
   def top
+    @columns = Column.all
+    @patches = Patch.order(created_at: :desc).limit(5)
+    @problems = Problem.order(created_at: :desc).limit(5)
+    @columns = Column.order(created_at: :desc).limit(5)
+    @topics = Topic.order(created_at: :desc).limit(5)
+    render :layout => 'top_visual'
   end
-  
+
 
 end
