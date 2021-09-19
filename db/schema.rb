bbd8ee8e415843ac44c0b29cd246a3bb7cdea356
pandaulait <show.hey.panda@gmail.com>
2021-09-19 00:08:01 +0000

    [Update]目次機能migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_092327) do
+ActiveRecord::Schema.define(version: 2021_09_19_000559) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -222,7 +222,7 @@ ActiveRecord::Schema.define(version: 2021_09_18_092327) do
 
   create_table "textbook_lists", force: :cascade do |t|
     t.integer "subject_area_id"
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["subject_area_id"], name: "index_textbook_lists_on_subject_area_id"
